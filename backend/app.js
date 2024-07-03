9a108ba5223df3636f06ba20d8313c0c7b196231
Tomislav Dukez <tomdu3@ymail.com>
2024-07-03 22:58:56 +0200

    add output gives server address

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.listen(3001, () => {
-    console.log('Server started on port 3001');
+    console.log(`Server started on  ${'http://localhost:3001'}`);
 });
