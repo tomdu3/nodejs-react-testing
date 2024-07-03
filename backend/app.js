7997a59408187e0fe6a70e310746084920cbe5b0
Tomislav Dukez <tomdu3@ymail.com>
2024-07-03 23:45:35 +0200

    add db connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`Server started on  ${'http://localhost:3001'}`);
-});
+const db = require('./models');
+
+db.sequelize.sync().then(() => {
+    console.log('Database connected');
+    app.listen(3001, () => {
+        console.log(`Server started on  ${'http://localhost:3001'}`);
+    });
+})
