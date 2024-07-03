5ef1e7e1af0af0f97452d24b35a2f0cd68975775
Tomislav Dukez <tomdu3@ymail.com>
2024-07-03 23:45:23 +0200

    add table restaurants

---
+++ backend/models/Restaurants.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Restaurants = sequelize.define("Restaurants", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    city: {
+      type: DataTypes.STRING,
+    },
+    state: {
+      type: DataTypes.STRING,
+    },
+  });
+  return Restaurants;
+};
