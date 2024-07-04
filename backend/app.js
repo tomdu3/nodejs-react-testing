acc5f637e44c8b912a83c66b04d901ba576b3ab6
Tomislav Dukez <tomdu3@ymail.com>
2024-07-04 13:21:08 +0200

    add test create record

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,35 @@ const app = express();
 
 const db = require('./models');
 
-db.sequelize.sync().then(() => {
+const { Restaurants } = require('./models');
+
+app.get('/select', (req, res) => {
+    res.send('select')
+})
+
+app.get('/insert', (req, res) => {
+    Restaurants.create({
+        name: 'Sushi Garden',
+        description: 'Japanese Restaurant',
+        address: '123 Main St',
+        city: 'New York',
+        state: 'NY'
+    }).catch(err => {
+        if (err) {
+        console.log(err);
+    }
+    })
+
+    res.send('insert')
+})
+
+app.get('/delete', (req, res) => {
+    res.send('delete')
+})
+db.sequelize.sync().then((req) => {
     console.log('Database connected');
     app.listen(3001, () => {
         console.log(`Server started on  ${'http://localhost:3001'}`);
     });
 })
+
